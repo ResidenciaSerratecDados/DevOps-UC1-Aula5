49335ec1dc7ee3f5262c176a3c34a40e8e4ec9b9
MURILO409 <murilodasdoresalves@gmail.com>
2025-04-28 20:51:28 -0300

    Adição de arquivos de TerraForm

---
+++ meu-projeto-terraform/main.tf
@@ -0,0 +1,12 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ServidorWeb"
+  }
+}
